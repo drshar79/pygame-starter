aaca0a98cacdd31c07688632397fc9935dafc022
Divyesh Sharma <drshar79>
2020-02-14 11:17:55 -0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -6,7 +6,10 @@ player = pygame.image.load('assets/hero/sliced/idle-1.png').convert_alpha()
 player=pygame.transform.scale(player,(50,50))
 BG=pygame.image.load("assets/BGimg.jpg").convert()
 BG=pygame.transform.scale(BG,(800,600))
+font=pygame.font.SysFont("arial", 30)
+
 hp=200
+text = font.render("HP going down", True, (255, 255, 255))
 
 x=400
 y=300
@@ -16,22 +19,25 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    if x >= 800:
-        hp -= 1
-        print(hp)
+
 
     win.fill((0, 0, 0))
     win.blit(BG,(0,60))
     win.blit(player, (x, y))
     keys=pygame.key.get_pressed()
     if keys[pygame.K_a]:
-        x-=.3
+        x-=.4
     if keys[pygame.K_d]:
-        x+=.3
+        x+=.4
     if keys[pygame.K_w]:
-        y-=.3
+        y-=.4
     if keys[pygame.K_s]:
-        y+=.3
+        y+=.4
+    if x >= 800 or x<=0:
+        hp -= 1
+        print(hp)
+    elif hp<=199:
+        win.blit(text, (400, 300))
 
 
     pygame.display.update()
